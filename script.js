d1d83dcdbd790afc0f2ab3977052911e9056c1ff
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-05-23 15:38:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+let input = document.getElementById("evaluatedText")
+
+input.addEventListener("keyup",()=>{
+  let h3 = document.getElementById("letterCount");
+  h3.innerText = input.value.length;
+})
